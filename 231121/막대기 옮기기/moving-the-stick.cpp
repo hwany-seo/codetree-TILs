5805087c478cd3f238c05d5048fb6b548406c427
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2023-11-21 22:18:02 +0900

    Title: 막대기 옮기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231121/막대기 옮기기/moving-the-stick.cpp
+++ 231121/막대기 옮기기/moving-the-stick.cpp
@@ -1,41 +1,45 @@
 #include<iostream>
 #include<cassert>
-using namespace std; 
+using namespace std;
 
 enum class eDir
 {
-	garo,sero,degak
+	garo, sero, degak
 };
 
-int N; 
-int Board[20][20]; 
+int N;
+int Board[20][20];
 
 bool CanGo(int row, int col)
 {
-	return Board[row][col] != 1; 
+	return Board[row][col] != 1;
 }
 
-int answer; 
-void dfs(int row, int col , const eDir d)
+int answer;
+int memo[3][20][20];
+int dfs(int row, int col, const eDir d)
 {
 	if (row == N && col == N) {
-		answer++; 
-		return; 
+		return 1;
 	}
 
-	if (CanGo(row + 1, col + 1) && CanGo(row + 1, col) && CanGo(row , col + 1)) {
-		dfs(row + 1, col + 1, eDir::degak); 
+	if (memo[(int)d][row][col] != -1) return memo[(int)d][row][col];
+
+	int cnt = 0;
+	if (CanGo(row + 1, col + 1) && CanGo(row + 1, col) && CanGo(row, col + 1)) {
+		cnt += dfs(row + 1, col + 1, eDir::degak);
 	}
 	if (d != eDir::sero) {
-		if (CanGo(row , col + 1)) {
-			dfs(row , col + 1, eDir::garo);
+		if (CanGo(row, col + 1)) {
+			cnt += dfs(row, col + 1, eDir::garo);
 		}
 	}
 	if (d != eDir::garo) {
-		if (CanGo(row + 1, col )) {
-			dfs(row + 1, col , eDir::sero);
+		if (CanGo(row + 1, col)) {
+			cnt += dfs(row + 1, col, eDir::sero);
 		}
 	}
+	return memo[(int)d][row][col] = cnt;
 }
 int main()
 {
@@ -44,15 +48,17 @@ int main()
 	for (int row = 0; row <= N + 1; row++) {
 		for (int col = 0; col <= N + 1; col++) {
 			Board[row][col] = 1;
+			memo[0][row][col] = -1;
+			memo[1][row][col] = -1;
+			memo[2][row][col] = -1;
 		}
 	}
 	for (int row = 1; row <= N; row++) {
 		for (int col = 1; col <= N; col++) {
-			cin >> Board[row][col]; 
+			cin >> Board[row][col];
 		}
 	}
 
-	dfs(1, 2, eDir::garo); 
-	cout << answer; 
-	return 0; 
+	cout << dfs(1, 2, eDir::garo);
+	return 0;
 }
