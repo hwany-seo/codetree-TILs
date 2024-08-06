6c39ca923c57e2ddc845815797d50f0635a11067
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2024-08-06 09:29:38 +0900

    Title: 계단 오르기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/계단 오르기/climbing-stairs.cpp
+++ 240806/계단 오르기/climbing-stairs.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 using namespace std; 
 
+int memo[1001]; 
+
+void initMemo() {
+	for (int i = 0; i < 1001; i++) {
+		memo[i] = -1; 
+	}
+}
+int NumberOfCase(int n) {
+	if (memo[n] != -1) return memo[n]; 
+	if (n == 0 || n == 2 || n == 3) return 1; 
+	return memo[n] = (NumberOfCase(n - 2) % 10007+ NumberOfCase(n - 3) % 10007) % 10007;
+}
 int main() {
 	int n;
 	cin >> n; 
 
-	long long dp[1001] = { 0 }; 
+	initMemo(); 
+	cout << NumberOfCase(n); 
+	/*int dp[1001] = { 0 }; 
 	dp[0] = 0; dp[1] = 0; 
 	dp[2] = 1; 
 	dp[3] = 1;
 
 	for (int i = 4; i <= n; i++) {
-		dp[i] = (dp[i - 2] % 10007 + dp[i - 3] % 10007) % 10007; 
+		dp[i] = dp[i - 2] + dp[i - 3]; 
 	}
 
-	cout << dp[n] % 10007;
+	cout << dp[n] % 10007;*/
 
 
 	return 0; 
