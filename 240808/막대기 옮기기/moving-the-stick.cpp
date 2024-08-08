10cdfd31bc13e689c1362a9be3bff5ef91d0c5d2
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2024-08-08 13:45:52 +0900

    Title: 막대기 옮기기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240808/막대기 옮기기/moving-the-stick.cpp
+++ 240808/막대기 옮기기/moving-the-stick.cpp
@@ -14,14 +14,15 @@ int main() {
 
 	const int STATE = 3; // 0 : 가로 , 1 : 세로 , 2 : 대각 
 	long long dp[MAXN + 1][MAXN + 1][STATE] = {0};
-	dp[2][1][1] = 1;
-	for (int row = 2; row <= n; row++) {
+	dp[1][2][0] = 1;
+	for (int row = 1; row <= n; row++) {
 		for (int col = 1; col <= n; col++) {
-			if (row == 2 && col == 1)continue; 
+			if (row == 1 && col == 2)continue; 
 			if (stage[row][col] != 0)continue; 
 			dp[row][col][0] = dp[row][col - 1][0] + dp[row][col - 1][2]; 
-			dp[row][col][1] = dp[row-1][col - 1][0] + dp[row-1][col - 1][1] + dp[row - 1][col - 1][2];
-			dp[row][col][2] = dp[row-1][col][1] + dp[row-1][col][2]; 
+			dp[row][col][1] = dp[row-1][col][1] + dp[row-1][col][2]; 
+			if (stage[row - 1][col] != 0 || stage[row][col - 1] != 0)continue; 
+			dp[row][col][2] = dp[row-1][col - 1][0] + dp[row-1][col - 1][1] + dp[row - 1][col - 1][2];
 		}
 	}
 	cout << dp[n][n][0] + dp[n][n][1] + dp[n][n][2] ; 
