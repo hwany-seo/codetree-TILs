a7ce8d35909152e08173bb23e10500f1b40ab6be
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2024-09-06 09:39:58 +0900

    Title: 동전 거슬러주기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240906/동전 거슬러주기/coin-change.cpp
+++ 240906/동전 거슬러주기/coin-change.cpp
@@ -7,51 +7,52 @@ dp: item을 적절히 고르는 문제
 */
 
 int N; // 동전 종류 
-int money; 
-std::vector<int> coins; 
+int money;
+std::vector<int> coins;
 int memo[10001];
 void init() {
-	std::cin >> N; 
-	std::cin >> money; 
-	coins = std::vector<int>(N); 
+	std::cin >> N;
+	std::cin >> money;
+	coins = std::vector<int>(N);
 	for (int i = 0; i < N; ++i) {
-		std::cin >> coins[i]; 
+		std::cin >> coins[i];
 	}
 	for (int i = 0; i <= 10000; i++) {
-		memo[i] = 1e9; 
+		memo[i] = 1e9;
 	}
 }
 
 /*
 * 문제에서 중요한것은 동전의 개수 / -> 추상화부분을 좀더 명확하게 서술할 수 있으면 좋을 거 같다.
-* 문제정의 
- MinCoinCount(금액 m) : 금액 m 을 동전으로 교환할 때 최소 동전 개수  
- 
+* 문제정의
+ MinCoinCount(금액 m) : 금액 m 을 동전으로 교환할 때 최소 동전 개수
+
 * 부분 문제와의 관계
  MinCoinCount(M) = min of MinCoinCount(M - coins[i]) + 1 (i는 모든 코인)
 */
 
 int MinCoinCount(int m) {
-	if (memo[m] != 1e9) return memo[m]; 
-	if (m <= 0) return 1e9; 
+	if (memo[m] != 1e9) return memo[m];
+	if (m == 0) return 0;
+	if (m < 0) return 1e9; 
 
-	int result = 1e9; 
+	int result = 1e9;
 	for (int i = 0; i < N; ++i) {
-		result = std::min(MinCoinCount(m - coins[i]), result); 
+		result = std::min(MinCoinCount(m - coins[i]), result);
 	}
 
-	if (result == 1e9) return result; 
+	if (result == 1e9) return result;
 	return memo[m] = result + 1;
 }
 int main() {
-	init(); 
-	int ret = MinCoinCount(money); 
-    if(ret == 1e9) { 
-        std::cout << "불가능" ; 
-    }
-    else { 
-        std::cout << ret ; 
+	init();
+	int ret = MinCoinCount(money);
+	if (ret == 1e9) {
+		std::cout << -1;
+	}
+	else {
+		std::cout << ret;
 
-    }
+	}
 
 }
