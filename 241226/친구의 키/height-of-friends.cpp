b1585eb49691552aebd86b3e1b5ae8c7e4ab1310
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2024-12-26 16:14:26 +0900

    Title: 친구의 키, Time: 54ms, Memory: 4MB, Status: Passed - CodeTree

--- 241226/친구의 키/height-of-friends.cpp
+++ 241226/친구의 키/height-of-friends.cpp
@@ -6,16 +6,13 @@
 using namespace std;
 
 int n, m;
-//vector<pair<int, int>> given_edges; 
 vector<int> edges[100'001];
-bool isNoRoot[100'001];
 void initTopologicalSort() {
 	cin >> n >> m;
 	for (int i = 0; i < m; ++i) {
 		int a, b;
 		cin >> a >> b;
 		edges[a].push_back(b);
-		isNoRoot[b] = true;
 	}
 }
 
@@ -35,7 +32,7 @@ int main() {
 
 	// dfs
 	for (int node = 1; node <= n; node++) {
-		if (isNoRoot[node])continue;
+		if (visited[node])continue; 
 		visited[node] = true;
 		dfs(node);
 	}
