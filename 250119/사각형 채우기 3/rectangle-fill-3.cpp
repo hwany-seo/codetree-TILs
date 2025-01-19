31c1adab5f77fd41e59bcd7032aa9b33a66130ee
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2025-01-19 22:40:38 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250119/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250119/사각형 채우기 3/rectangle-fill-3.cpp
@@ -16,13 +16,11 @@ int main() {
     const long long DIV = 1'000'000'007;
     for(int i = 3; i <= n; i ++) { 
         dp[i] = (dp[i-1] * 2 % DIV+ dp[i-2] * 3 % DIV) % DIV;
-        if(i % 2 == 0){ 
-            dp[i] += 4; 
-        }
-        else { 
+
+        for(int t = i-3; t >= 0; t --) { 
             dp[i] += 2; 
-        }
-        dp[i] %= DIV; 
+            dp[i] %= DIV; 
+        } 
     }
 
     cout << dp[n];
