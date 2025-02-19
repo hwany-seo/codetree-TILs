b0438c3ffe403626ca88d1f42563e40dc930483e
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2025-02-19 20:16:46 +0900

    Title: 데이터 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250219/데이터 비교/data-comparison.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr1[100000];
+int arr2[100000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> arr2[i];
+    }
+
+    // Write your code here!
+
+    return 0;
+}
