a10090c5368d541c6bc41ce345c6a3e1112362d1
hwany-seo <53638327+hwany-seo@users.noreply.github.com>
2025-02-19 20:36:32 +0900

    Title: hashset 기본, Time: 62ms, Memory: 3MB, Status: Passed - CodeTree

--- 250219/hashset 기본/hashset-basic.cpp
+++ 250219/hashset 기본/hashset-basic.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
 #include <unordered_set>
+#include <string>
 using namespace std;
 
 int n;
 string command[100000];
 int x[100000];
 
-constexpr unsigned int Hash(string str) { 
-    str[0] ? static_cast<unsigned int>(str[0]) + 33 * Hash(str + 1) : 5381;
+constexpr unsigned int Hash(const char* str) {
+    unsigned int hash = 5381;
+    while (*str) {
+        hash = ((hash << 5) + hash) + *str++;
+    }
+    return hash;
 }
+
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> command[i] >> x[i];
     }
 
-    unordered_set<int> s; 
-    for (int i = 0; i < n; i ++){ 
-        switch(Hash(command[i])){ 
-            case Hash("find"):
-                if(s.find(x[i]) != s.end()){ 
-                    cout << "true" << endl; 
-                }
-                else { 
-                    cout << "false" << endl; 
-                }
-                break; 
-            case Hash("add") :
-                s.insert(x[i]); 
-                break; 
-            case Hash("remove"):
-                s.erase(x[i]); 
-                break; 
+    unordered_set<int> s;
+    for (int i = 0; i < n; i++) {
+        switch (Hash(command[i].c_str())) {
+        case Hash("find"):
+            if (s.find(x[i]) != s.end()) {
+                cout << "true" << endl;
+            }
+            else {
+                cout << "false" << endl;
+            }
+            break;
+        case Hash("add"):
+            s.insert(x[i]);
+            break;
+        case Hash("remove"):
+            s.erase(x[i]);
+            break;
         }
     }
 
